041dcdf6f50e7e4f6e674b2fb79037b5eae7a8c5
Eyup Han <eyup57066@gmail.com>
2025-01-22 15:19:47 +0100

    Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -3,4 +3,15 @@
 const boutiquedusorcier = "La Boutique du Sorcier";
 let potionsdesoin_stock = 20;
 let potionsdesoin_prix = 5;
-let boutiquedusorcier_statut = true;
+let est_boutique_du_sorcier_ouverte = false;
+
+///Exercice 2
+
+if (est_boutique_du_sorcier_ouverte)
+  console.log("Bienvenue dans la boutique", boutiquedusorcier, "Aventurier !");
+else
+  console.log(
+    "La boutique",
+    boutiquedusorcier,
+    "est fermée, revenez plus tard Aventurier !"
+  );
